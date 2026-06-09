5c833a5265a872989b2d7ce129a5d51bb2523b1e
ShathruknaS <shathruknas94@gmail.com>
2026-06-09 20:57:03 +0530

    Fix media serving and add default profile pic to git

--- instagram_clone/urls.py
+++ instagram_clone/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.shortcuts import redirect
+
 
 def root_redirect(request):
     if request.user.is_authenticated:
         return redirect("feed")
     return redirect("login")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -20,5 +22,7 @@ urlpatterns = [
     path("reels/", include("reels.urls")),
 ]
 
-# Serve media files in all environments
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
