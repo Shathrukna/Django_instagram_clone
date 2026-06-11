73555f793439ebab314db139ed79ab29f0be0147
ShathruknaS <shathruknas94@gmail.com>
2026-06-11 22:26:52 +0530

    Fix middleware 500 error

--- users/middleware.py
+++ users/middleware.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LastSeenMiddleware:
@@ -8,6 +8,5 @@ class LastSeenMiddleware:
 
     def __call__(self, request):
         if request.user.is_authenticated:
-            Profile = User.profile.__class__
             Profile.objects.filter(user=request.user).update(last_seen=timezone.now())
         return self.get_response(request)
